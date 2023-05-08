9ab8ed53927a9d06fe5f47e01515bc56f76afeeb
hsgh085 <hsyu0917@gmail.com>
2023-05-08 18:00:09 +0900

    test

---
+++ src/screen/expenditure/ExpenditureMonth.js
@@ -0,0 +1,11 @@
+import { Platform,StyleSheet, Text, View, Button,Alert } from 'react-native';
+
+const ExpenditureMonth = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ExpenditureMonth;
---
+++ src/screen/memo/MemoDetail.js
@@ -0,0 +1,5 @@
+const MemoDetail = () => {
+  return <div></div>;
+};
+
+export default MemoDetail;
