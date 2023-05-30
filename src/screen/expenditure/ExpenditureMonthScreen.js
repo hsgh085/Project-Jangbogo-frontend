1f157906def1d9098ed8637da630503c53df7c8e
hsgh085 <hsyu0917@gmail.com>
2023-05-31 04:59:44 +0900

    지출내역 그래프

--- src/screen/expenditure/ExpenditureMonthScreen.js
+++ src/screen/expenditure/ExpenditureMonthScreen.js
@@ -1,14 +1,60 @@
-import React from 'react';
-import { View,Text } from 'react-native';
+import React, { useCallback, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import DateTimePicker from "react-native-modal-datetime-picker";
+import { Feather} from "@expo/vector-icons";
+import colors from '../../../assets/colors/colors';
+
 
 const ExpenditureMonthScreen = () => {
-    return (
-        <View>
-            <Text>
-                일 별 지출내역
-            </Text>
-        </View>
-    );
+  const [visibleDatePicker, setVisibleDatePicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const onPressCalendar = useCallback(() => {
+    setVisibleDatePicker(true);
+  });
+  return (
+    <View style={s.container}>
+      <View style={s.dateContainer}>
+        <Text style={s.dateText}>
+          {`${selectedDate.getFullYear()}.${
+            selectedDate.getMonth() + 1
+          }.${selectedDate.getDate()}`}
+        </Text>
+        <Feather
+          name="calendar"
+          size={18}
+          color={colors.red}
+          onPress={onPressCalendar}
+        />
+      </View>
+      <DateTimePicker
+        isVisible={visibleDatePicker}
+        mode="date"
+        onConfirm={(date) => {
+          setSelectedDate(new Date(date));
+          setVisibleDatePicker(false);
+        }}
+        onCancel={() => {
+          setVisibleDatePicker(false);
+        }}
+      />
+    </View>
+  );
 };
 
 export default ExpenditureMonthScreen;
+
+const s = StyleSheet.create({
+  container: {
+    padding: 30,
+  },
+  dateContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  dateText: {
+    fontSize: 17,
+    color: colors.gray,
+    marginRight: 2,
+  },
+});
--- src/screen/expenditure/ExpenditureYearScreen.js
+++ src/screen/expenditure/ExpenditureYearScreen.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Dimensions, ScrollView } from "react-native";
 import { Picker } from "@react-native-picker/picker";
-import colors from '../../../assets/colors/colors';
+import colors from "../../../assets/colors/colors";
+import { BarChart } from "react-native-chart-kit";
 
 const ExpenditureYearScreen = () => {
   const [selectedYear, setSelectedYear] = useState();
@@ -10,7 +11,7 @@ const ExpenditureYearScreen = () => {
       <Picker
         selectedValue={selectedYear}
         onValueChange={(itemValue, itemIndex) => setSelectedYear(itemValue)}
-      style={s.selectYear}
+        style={s.selectYear}
       >
         <Picker.Item label="2023년" value="2023" />
         <Picker.Item label="2022년" value="2022" />
@@ -18,6 +19,56 @@ const ExpenditureYearScreen = () => {
         <Picker.Item label="2020년" value="2020" />
         <Picker.Item label="2019년" value="2019" />
       </Picker>
+      <ScrollView horizontal={true} style={s.chartConatiner}>
+        <BarChart
+          data={{
+            labels: [
+              "1월",
+              "2월",
+              "3월",
+              "4월",
+              "5월",
+              "6월",
+              "7월",
+              "8월",
+              "9월",
+              "10월",
+              "11월",
+              "12월",
+            ],
+            datasets: [
+              {
+                data: [20, 45, 28, 80, 99, 43, 77, 62, 12, 10, 90, 50],
+              },
+            ],
+          }}
+          width={550}
+          height={Dimensions.get("window").width - 50}
+          fromZero={true}
+          showValuesOnTopOfBars={true}
+          yAxisSuffix={"K"}
+          horizontalLabelRotation={270}
+          verticalLabelRotation={270}
+          chartConfig={{
+            barPercentage: 0.5,
+            backgroundColor: colors.white,
+            backgroundGradientFrom: "#FAFAFA",
+            backgroundGradientTo: "#FAFAFA",
+            fillShadowGradient: colors.greenH,
+            fillShadowGradientOpacity: 1,
+            decimalPlaces: 2, //소수점 자리수
+            color: (opacity = 1) => `rgba(102, 102, 102, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          style={{
+            marginVertical: 10,
+            borderRadius: 16,
+            flex: 1,
+          }}
+        />
+      </ScrollView>
     </View>
   );
 };
@@ -25,11 +76,14 @@ const ExpenditureYearScreen = () => {
 export default ExpenditureYearScreen;
 
 const s = StyleSheet.create({
-    container:{
-        padding:15,
-    },
-    selectYear:{
-        width:140,
-        color:colors.gray,
-    },
+  container: {
+    padding: 15,
+  },
+  selectYear: {
+    width: 140,
+    color: colors.gray,
+  },
+  chartConatiner: {
+    transform: [{ rotate: "90deg" }],
+  },
 });
