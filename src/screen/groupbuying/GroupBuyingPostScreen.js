938308e65bccd6554e4aa06af669fb36ef86595e
hsgh085 <hsyu0917@gmail.com>
2023-06-12 17:53:25 +0900

    공동구매 등록 헤더

--- src/screen/groupbuying/GroupBuyingPostScreen.js
+++ src/screen/groupbuying/GroupBuyingPostScreen.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Header from '../../components/Header/Header';
+import colors from '../../../assets/colors/colors';
 
 const GroupBuyingPostScreen = () => {
   return (
     <>
-      <View>
-        <Text>등록화면</Text>
+      <View style={{ flex: 1, backgroundColor: colors.white }}>
+        <Header>
+            <View style={s.postBtn}>
+                <Text>등록</Text>
+            </View>
+        </Header>
       </View>
     </>
   );
 };
 
 export default GroupBuyingPostScreen;
+
+const s=StyleSheet.create({
+    postBtn:{
+        paddingVertical:10,
+        paddingHorizontal:25,
+        backgroundColor: colors.greenH,
+        borderRadius:20,
+    },
+});
