a99d04e8b6fae282e71dcc87a141854275344713
hsgh085 <hsyu0917@gmail.com>
2023-06-21 02:59:04 +0900

    타이머 에러 수정

--- src/screen/groupbuying/_com/EndTimer.js
+++ src/screen/groupbuying/_com/EndTimer.js
@@ -9,7 +9,8 @@ const EndTimer = (props) => {
   const diff = Math.max(0, end - now); // 음수일 경우 0으로 설정
   const seconds = Math.floor((diff / 1000) % 60);
   const minutes = Math.floor((diff / (1000 * 60)) % 60);
-  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24)); // diff 값의 전체 일수 계산
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24) + days * 24; // 전체 일수에 따른 hours 계산
   const [timer, setTimer] = useState(diff);
   const [timeOut, setTimeOut] = useState(false);
 
@@ -53,9 +54,9 @@ const EndTimer = (props) => {
 
   return (
     <>
-      <Text style={{ fontSize: 12, fontWeight: 500, color: timer < 3600000 ? colors.red : colors.green }}>{`${hours
-        .toString()
-        .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}</Text>
+      <Text style={{ fontSize: 12, fontWeight: 500, color: timer < 3600000 ? colors.red : colors.green }}>
+        {`${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}
+      </Text>
     </>
   );
 };
